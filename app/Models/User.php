0258a060ed865b4ef57e6ee56fc98c98d64b6061
vittorio pinti <vittorio.pinti@gmail.com>
2023-06-25 17:58:45 +0200

    feat: user profile

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,9 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Role::class);
     }
+
+    public function profile()
+    {
+        return $this->hasOne(UserProfile::class);
+    }
 }
